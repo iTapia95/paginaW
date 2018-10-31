46f71a7006cb2d3fb1d9296ea809721e01366a8d
Rajomami <rajomami@gmail.com>
2018-10-31 16:51:23 -0600

    pinche victor manco

--- controllers/encuestaController.js
+++ controllers/encuestaController.js
@@ -3,7 +3,9 @@ const dbConnection = require('../config/BDConnection');
 const index = (req, res) => {
 	res.render('Respuestas');
 }
-
+const read = (req, res) => {
+	res.render('para el push');
+}
 module.exports = {
 	index
 }
